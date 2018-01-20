5bf0c98cc05f6c093f5a331ee441d39ccc6dfccb
Sherwin Mina <sherwinmina@yahoo.com>
2018-01-20 12:16:27 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from './App.css'
+
+import Button from './components/Button/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button primary>Hello</Button>
+      </div>
+    )
+  }
+}
+
+export default App
